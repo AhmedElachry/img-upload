90aa73f318e415c1cf625fec08eb583f873c1b95
Ahmed Elashry <ahmedelashry.yt@gmail.com>
2023-09-06 13:15:31 +0300

    added image preview and some improvments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [selectedImages, setSelectedImages] = useState([]);
+  const [imgPreview, setImgPreview] = useState("");
 
   useEffect(() => {
     setSelectedImages([
@@ -19,42 +20,54 @@ function App() {
     setSelectedImages((previousImages) =>
       previousImages.concat(imagesUrlsArray)
     );
-    e.target.value = "";
+    console.log(selectedFiles);
+    console.log(selectedFilesArray);
+    console.log(imagesUrlsArray);
+  };
+
+  const handleDelete = (imgUrl) => {
+    URL.revokeObjectURL(imgUrl);
+    setSelectedImages(
+      selectedImages.filter((imgToDelete) => {
+        return imgToDelete !== imgUrl;
+      })
+    );
+    imgPreview === imgUrl && setImgPreview("");
   };
 
   return (
     <div className="App">
-      <section>
-        <label>
-          + Add Images
-          <br />
-          <input
-            type="file"
-            name="images"
-            onChange={onSelectFiles}
-            multiple
-            accept="image/png , image/jpeg"
-          />
-        </label>
-      </section>
-      <br />
-      <div className="images">
-        {selectedImages.map((imgUrl) => (
-          <div key={imgUrl} className="image">
-            <img src={imgUrl} height="200" width="200" alt="uploaded" />
-            <button
-              onClick={() =>
-                setSelectedImages(
-                  selectedImages.filter((p) => {
-                    return p !== imgUrl;
-                  })
-                )
-              }
-            >
-              delete
-            </button>
-          </div>
-        ))}
+      <div className="uploader">
+        <section>
+          <label>
+            + Add Images
+            <br />
+            <input
+              type="file"
+              name="images"
+              onChange={onSelectFiles}
+              multiple
+              accept="image/png , image/jpeg"
+            />
+          </label>
+        </section>
+        <br />
+        <div className="images">
+          {selectedImages.map((imgUrl) => (
+            <div key={imgUrl} className="image">
+              <img src={imgUrl} height="100" width="100" alt="uploaded" />
+              <button onClick={() => handleDelete(imgUrl)}>delete</button>
+              <div className="middle">
+                <div className="text" onClick={() => setImgPreview(imgUrl)}>
+                  preview
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="image-previewer">
+        {imgPreview && <img src={imgPreview} alt="imgPreview" width={"50%"} />}
       </div>
     </div>
   );
